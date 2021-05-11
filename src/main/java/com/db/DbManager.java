d2704eb20fd16f62b84a955befdf304ed6586d48
SkylineSG <Sebastian97x@gmail.com>
2021-05-11 19:47:01 +0200

    connecting MySql database

---
+++ src/main/java/com/db/DbManager.java
@@ -0,0 +1,33 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DbManager {
+    private Connection conn;
+    private static DbManager dbManagerInstance;
+
+    private DbManager() throws SQLException {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "carRent_user");
+        connectionProps.put("password", "Pusia13x");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/rentCars?serverTimezone=Europe/Warsaw" +
+                        "&useSSL=False",
+                connectionProps);
+    }
+
+    public static DbManager getInstance() throws SQLException {
+        if (dbManagerInstance == null) {
+            dbManagerInstance = new DbManager();
+        }
+        return dbManagerInstance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
+
---
+++ src/test/java/com/db/DbManagerTestSuite.java
@@ -0,0 +1,17 @@
+//package com.db;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//
+//import java.sql.SQLException;
+//
+//public class DbManagerTestSuite {
+//    @Test
+//    public void testConnect() throws SQLException {
+//        //Given
+//        //When
+//        DbManager dbManager = DbManager.getInstance();
+//        //tHEN
+//        Assertions.assertNotNull(dbManager.getConnection());
+//    }
+//}
